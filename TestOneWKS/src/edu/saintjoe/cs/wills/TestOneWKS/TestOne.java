0ec23407b255b47401dfa775be0cb0571e5fa122
wills <wills@localhost>
2016-02-26 14:30:09 -0500

    First Commit

--- TestOneWKS/src/edu/saintjoe/cs/wills/TestOneWKS/TestOne.java
+++ TestOneWKS/src/edu/saintjoe/cs/wills/TestOneWKS/TestOne.java
@@ -3,7 +3,7 @@ package edu.saintjoe.cs.wills.TestOneWKS;
 public class TestOne {
 
 	public static void main(String[] args) {
-		// App made for test purpose 
+		// App made for test purposes
 	}
 
 }
